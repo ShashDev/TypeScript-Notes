a3eb6cd0a3cba8b84ed7e0b5a769e15e46d8d98d
Shashwat <shashwatbhadoriya17@gmail.com>
2023-03-25 18:35:16 +0530

    p

--- main.js
+++ main.js
@@ -1,48 +1,17 @@
-var employees = [
-    {
-        ID: 1,
-        Firstname: 'Person1',
-        Lastname: 'Person1',
-        Address: 'location',
-        salary: 1234,
-    },
-    {
-        ID: 2,
-        Firstname: 'Person2',
-        Lastname: 'Person2',
-        Address: 'location',
-        salary: 4567,
-    },
-    {
-        ID: 3,
-        Firstname: 'Person3',
-        Lastname: 'Person3',
-        Address: 'location',
-        salary: 7890,
-    },
-    {
-        ID: 4,
-        Firstname: 'Person4',
-        Lastname: 'Person4',
-        Address: 'location',
-        salary: 3456,
-    },
-    {
-        ID: 5,
-        Firstname: 'Person5',
-        Lastname: 'Person5',
-        Address: 'location',
-        salary: 45366,
-    },
-];
-var placeholder = document.getElementById('tbody');
-var out = "";
-for (var _i = 0, employees_1 = employees; _i < employees_1.length; _i++) {
-    var i = employees_1[_i];
-    out += "\n   <tr>\n           <td>".concat(i.ID, "</td>\n           <td>").concat(i.Firstname, "</td>\n           <td>").concat(i.Lastname, "</td>\n           <td>").concat(i.Address, "</td>\n           <td>").concat(i.salary, "</td>\n         </tr>\n   ");
+// interface User{
+//   name:string
+//   age:number,
+//   salary? :number,
+//   getdata:()=>string
+// };
+// var student:User={
+// name:"shashwat",
+// age:21, 
+// salary:400000000000000000,
+// getdata:()=>student.name
+// }
+;
+function student(p) {
+    return "Name " + p.name + " Age " + p.age + " Salary " + p.salary;
 }
-placeholder.innerHTML = out;
-function search() {
-    console.log("a");
-}
-var a = "hello";
+console.log(student({ name: "shashwat", age: 21, salary: 400000 }));
--- main.ts
+++ main.ts
@@ -1,73 +1,53 @@
-var employees: {
-    ID: number;
-    Firstname: string;
-    Lastname: string;
-    Address: string;
-    salary: number;
-  }[] = [
-      {
-        ID: 1,
-        Firstname: 'Person1',
-        Lastname: 'Person1',
-        Address: 'location',
-        salary: 1234,
-      },
-      {
-        ID: 2,
-        Firstname: 'Person2',
-        Lastname: 'Person2',
-        Address: 'location',
-        salary: 4567,
-      },
-      {
-        ID: 3,
-        Firstname: 'Person3',
-        Lastname: 'Person3',
-        Address: 'location',
-        salary: 7890,
-      },
-      {
-        ID: 4,
-        Firstname: 'Person4',
-        Lastname: 'Person4',
-        Address: 'location',
-        salary: 3456,
-      },
-      {
-        ID: 5,
-        Firstname: 'Person5',
-        Lastname: 'Person5',
-        Address: 'location',
-        salary: 45366,
-      },
-    ];
-  
-  
-  
-      
-  var placeholder:any=<HTMLElement>document.getElementById('tbody');
-  var out=""
-  for(let i of employees){
-   out+=`
-   <tr>
-           <td>${i.ID}</td>
-           <td>${i.Firstname}</td>
-           <td>${i.Lastname}</td>
-           <td>${i.Address}</td>
-           <td>${i.salary}</td>
-         </tr>
-   `
-  }
-  placeholder.innerHTML=out
-    
-  
-    
-   function search(){
-    console.log("a")
-  }
-  
-  const a = "hello"
-  
-  
-  
-  
+// interface User{
+//   name:string
+//   age:number,
+//   salary? :number,
+//   getdata:()=>string
+// };
+// var student:User={
+// name:"shashwat",
+// age:21, 
+// salary:400000000000000000,
+// getdata:()=>student.name
+// }
+
+
+// console.log(student.getdata());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+interface User{
+    name:string
+    age:number,
+    salary? :number,
+    // getdata:()=>string
+  };
+
+function student(p:User) {
+  return "Name "+p.name + " Age "+p.age +" Salary "+p.salary
+}
+
+ console.log(  student({name:"shashwat",age:21,salary:400000})
+ );
+
+
+
+
+
+
+
+
+
